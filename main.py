36a5fef28eb94801657f5353a64876444b0e4d5e
hustinooo21 <reyes.justin2121@gmail.com>
2025-09-14 22:59:21 +0800

    try

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("Hello World!")
 print("Group 5 Members:")
 print("Justin Reyes - The POGI")
+print("Juneiah Psalm Paredes - Pinaka masipag")
+print("Calvin Manaor - Pinaka mabait")
+print("Nash Wynsjohn Bautista - The one and only.")
+print("John Doe")
+
